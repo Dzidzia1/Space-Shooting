8755c61a45e1e1261164dfb9af5f1c49f0ccc215
Marta <mkosior789@gmail.com>
2021-10-03 13:48:36 +0200

    Initial commit

---
+++ Assets/Scripts/CoinSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour
+{
+    public float TimeBetweenSpawns;
+    public GameObject prefab;
+
+    private float TimeToNextSpawn;
+
+    void Start()
+    {
+        TimeToNextSpawn = TimeBetweenSpawns;
+    }
+    
+    void Spawn()
+    {
+        for(int i=0; i<2; i++)
+        Instantiate(prefab,new Vector2(-2+ 7f*i, 10),Quaternion.identity);
+    }
+
+    void Update()
+    {
+        TimeToNextSpawn -= Time.deltaTime;
+        if(TimeToNextSpawn<0.0f)
+        {
+            Spawn();
+            TimeToNextSpawn = TimeBetweenSpawns;
+        }
+    }
+}
